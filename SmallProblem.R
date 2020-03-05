30c7953f2c68509707589055666f5d85c75ea95c
Joon <joon.durandt@gmail.com>
2020-03-05 12:06:59 +0000

    minor changes

--- SmallProblem.R
+++ SmallProblem.R
@@ -1,17 +1,16 @@
 # Create two character vectors for testing
 vec1 <- c("Hockey", "Football", "Baseball", "Curling", "Rugby", "Hurling", 
           "Basketball", "Tennis", "Cricket", "Lacrosse")
-vec2 <- c("Hockey", "Lacrosse", "Hockey", "Water Polo", "Hockey", "Lacrosse")
-
-# Concatenate the two vectors
-c(vec1, vec2)
+vec2 <- c(vec1, "Hockey", "Lacrosse", "Hockey", "Water Polo", "Hockey", "Lacrosse")
 
 # Extract some items from vec2 using a vector of indices
 vec3 <- vec1[c(1, 3, 6)]
 # Extract some items from vec2 using the individual items
 vec3_alt <- c(vec1[1], vec1[3], vec1[6])
 
-# Create a factor of the extracted text and display
+# Create a factor of the extracted text and explore type
 vec3_factor = as.factor(vec3)
 class(vec3_factor)
+class(vec3)
 vec3_factor
+
